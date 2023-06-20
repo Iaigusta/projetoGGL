6a7ee8473e2396d25aea8d9edcf66ff621be6959
Iaigusta <129605280+Iaigusta@users.noreply.github.com>
2023-06-20 19:36:55 -0300

    Add files via upload

--- index3.php
+++ index3.php
@@ -1,23 +1,32 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <link rel="stylesheet" href="css/styles.css">
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Incrições</title>
 </head>
 <body>
-    <h1>QUAL A SUA IDADE?</h1>
-    <div class="form-idade">
+   <section>
+    <div class="form-idade"> 
+        <div class="form-idade2">
+        <h1>QUAL A SUA IDADE?</h1>
     <form method="post" action="tentativa.php">
-        <select name="idade">
-            <option value="5a10">5->10</option>
-            <option value="10a15">10->15</option>
-            <option value="15a18">15->18</option>
-            <option value="18mais">18+</option>
-        </select>
-        <input type="submit" value="Enviar">
+    <input id="idade" type='range' value='50' max='100'>
+    <span id="s_idade">50</span>
+    <script>
+    var $range = document.querySelector('#idade'),
+        $value = document.querySelector('#s_idade');
+
+    $range.addEventListener('input', function() {
+    $value.textContent = this.value;
+    });
+    </script>
+    <input type="submit" value="enviar">
     </form>
     </div>
+    </div>
+    </section>
 </body>
-</html>
+</html> 
